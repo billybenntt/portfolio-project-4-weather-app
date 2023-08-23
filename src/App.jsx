3a0f89dad38a72540413ac49c46cc0e4097ec765
Bill Bennett <billbenntt@gapp.nthu.edu.tw>
2023-08-23 11:46:32 +0800

    worked layout for mobile size

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,38 @@ function App () {
   return (
     <main className="main">
       <div className="main-center">
-
         <div className="location">
-          <div className="location--header">
+          <div className="location--group-1">
             <div>search</div>
             <div>current location</div>
           </div>
-          <div className="location--status">
+          <div className="location--group-2">
             <Icon url="cloudy-day-3"/>
+          </div>
+
+          <div className="location--group-3">
+            <h2>15c</h2>
+            <h3>Shower</h3>
           </div>
         </div>
 
         <div className="forecast">
-          <div>forecast item</div>
-          <div>forecast item</div>
-          <div>forecast item</div>
-          <div>forecast item</div>
-          <div>forecast item</div>
+          <div className="forecast-center">
+            <div className="forecast-item">forecast item</div>
+            <div className="forecast-item">forecast item</div>
+            <div className="forecast-item">forecast item</div>
+            <div className="forecast-item">forecast item</div>
+            <div className="forecast-item ra">forecast item</div>
+          </div>
         </div>
 
         <div className="highlights">
-          <div>highlights item</div>
-          <div>highlights item</div>
-          <div>highlights item</div>
-          <div>highlights item</div>
+          <div className="highlights-center">
+            <div className="highlights-item">highlights item</div>
+            <div className="highlights-item">highlights item</div>
+            <div className="highlights-item">highlights item</div>
+            <div className="highlights-item">highlights item</div>
+          </div>
         </div>
 
       </div>
